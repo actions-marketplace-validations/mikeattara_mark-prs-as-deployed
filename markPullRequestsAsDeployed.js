7488ff5bc32473bc6c0d4243e68a8455891148b9
Mike Perry Y Attara <mpyebattara@gmail.com>
2021-03-04 21:18:13 +0000

    initial action

---
+++ markPullRequestsAsDeployed.js
@@ -0,0 +1,45 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const PR_LIST = JSON.parse(core.getInput('PR_LIST', {required: true}));
+const IS_PRODUCTION_DEPLOY = JSON.parse(core.getInput('IS_PRODUCTION_DEPLOY', {required: true}));
+const DATE = new Date();
+// eslint-disable-next-line max-len
+const MESSAGE = `Deployed to ${IS_PRODUCTION_DEPLOY ? 'production' : 'staging'} on ${DATE.toDateString()} at ${DATE.toTimeString()}`;
+
+const octokit = github.getOctokit(github.token);
+
+/**
+ * Create comment on pull request
+ *
+ * @param {Number} number - The pull request number
+ * @returns {Promise}
+ */
+async function createComment(number) {
+    console.log(`Writing comment on #${number}`);
+    try {
+        const response = await octokit.issues.createComment({
+            ...github.context.repo,
+            issue_number: number,
+            body: MESSAGE,
+        });
+        return response;
+    } catch (error) {
+        console.log(`Unable to write comment on #${number}`);
+        core.setFailed(error);
+    }
+}
+
+/**
+ * Action runner function
+ *
+ */
+function run() {
+    PR_LIST.forEach((pr) => {
+        createComment(pr);
+    });
+}
+
+run();
+
+module.exports = {createComment};
